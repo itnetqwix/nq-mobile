61541e9c16364891a2f73c561dcb0092997c4254
Kumar Satyam <ksatyam.1245@gmail.com>
2026-06-17 14:43:23 +0530

    Fix import path for colors in NotificationBellButton component

--- src/features/notifications/components/NotificationBellButton.tsx
+++ src/features/notifications/components/NotificationBellButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Pressable, StyleSheet, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { colors } from "../../theme";
+import { colors } from "../../../theme";
 
 type Props = {
   onPress: () => void;
